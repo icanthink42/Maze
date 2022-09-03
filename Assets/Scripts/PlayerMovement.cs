b23ea47d392532f7bac2750beace9ba99589a347
jackn <neelemanjack@gmail.com>
2022-09-02 22:59:30 -0400

    Made player movement work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController controller;
-    private float playerSpeed = -1.0f;
+    private Rigidbody2D rb;
+    [SerializeField] float playerSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        Physics.gravity = Vector3.zero;
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        float x = playerSpeed*Input.GetAxis ("Horizontal");
-        float y = playerSpeed*Input.GetAxis ("Vertical");
-        controller.Move(new Vector3(x,y,0));   
-        // Debug.Log("x: "+x);
-        // Debug.Log("y: "+y);
+    {   
+        float x = playerSpeed * Input.GetAxis ("Horizontal") * Time.fixedTime;
+        float y = playerSpeed * Input.GetAxis ("Vertical") * Time.fixedTime;
+        rb.velocity = new Vector2(x, y);
     }
 }
