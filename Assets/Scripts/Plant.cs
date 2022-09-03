644082aceda04a740416d169285eb9f00200f02b
robobruser <paulbruzzi@gmail.com>
2022-09-03 16:47:39 -0400

    plant creation

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField] private GameObject text;
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.transform.CompareTag("player")){
+            text.GetComponent<TextAnimation>().ShowErrorText("Level Complete!");
+        }
+            
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
