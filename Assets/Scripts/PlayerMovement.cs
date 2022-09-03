e0a4a587e3aa3935d714f72f6a4cbf2ae8db6acc
jackn <neelemanjack@gmail.com>
2022-09-03 12:58:39 -0400

    Added teleporting holes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -51,4 +53,12 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        HoleTpManager hm = col.gameObject.GetComponent<HoleTpManager>();
+        if (hm != null)
+        {
+            transform.position = hm.endLocation.position;
+        }
+    }
 }   
