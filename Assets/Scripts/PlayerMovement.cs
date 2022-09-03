5befd996a9bbf98f95f8bab27ae9f4ec1f74fb9f
robobruser <paulbruzzi@gmail.com>
2022-09-02 22:05:32 -0400

    Movement wo camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
+    private float playerSpeed = -1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float x = playerSpeed*Input.GetAxis ("Horizontal");
+        float y = playerSpeed*Input.GetAxis ("Vertical");
+        controller.Move(new Vector3(x,y,0));   
+        // Debug.Log("x: "+x);
+        // Debug.Log("y: "+y);
     }
 }
