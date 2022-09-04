55d43add2b9f4a9a3f336094f3168e2293e449f7
jackn <neelemanjack@gmail.com>
2022-09-04 14:34:28 -0400

    Improved vision manager

--- Assets/Scripts/SimpleView.cs
+++ Assets/Scripts/SimpleView.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleView : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class SimpleView : MonoBehaviour
         GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
         foreach (var obj in allObjects)
         {
-            if (obj.transform.CompareTag("player"))
+            if (obj.transform.CompareTag("player") || obj.transform.CompareTag("nohide"))
             {
                 continue;
             }
