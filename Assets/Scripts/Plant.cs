d92f19757e8859a69819b5cee4db64ddaafafaf5
robobruser <paulbruzzi@gmail.com>
2022-09-04 14:29:10 -0400

    Menus

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -6,11 +6,10 @@ using UnityEngine.SceneManagement;
 public class Plant : MonoBehaviour
 {
     [SerializeField] private GameObject text;
-    public string nextLevel;
     void OnTriggerEnter2D(Collider2D other){
         if (other.transform.CompareTag("player")){
             text.GetComponent<TextAnimation>().ShowErrorText("Level Complete!");
-            SceneManager.LoadScene(nextLevel); 
+            SceneManager.LoadScene("LevelSelect"); 
         }
     }
     // Start is called before the first frame update
